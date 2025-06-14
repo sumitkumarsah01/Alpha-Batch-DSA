fc1f665bf22213d78cd70e3b97a8ed72ad3a6bd7
Sumit Kumar Sah <sumitkumar92365gmail.com>
2025-06-14 06:45:55 +0530

    Input from User in Array

---
+++ Array/inputOutput.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class inputOutput {
+    public static void main(String[] args) {
+        int marks[]=new int[20];
+        Scanner sc=new Scanner(System.in);
+
+
+
+        System.out.println(marks.length);
+        // marks[0]=sc.nextInt();
+        // marks[1]=sc.nextInt();
+        // marks[2]=sc.nextInt();
+
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+
+        // marks[2]=100;
+        // ;System.out.println("maths = "+marks[2]);
+
+        
+    }
+    
+}
