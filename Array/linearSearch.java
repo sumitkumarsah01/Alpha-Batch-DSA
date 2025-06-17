71226a9354093383008ecc03eaabefd3e6367584
Sumit Kumar Sah <sumitkumar92365gmail.com>
2025-06-17 11:32:47 +0530

    Linear Search

--- Array/linearSearch.java
+++ Array/linearSearch.java
@@ -1,15 +1,16 @@
 public class linearSearch {
-    public static void linear(int arr[],int num){
+    public static int linear(int arr[],int key){
         for(int i=0;i<arr.length;i++){
-            if(arr[i]==num){
-                System.out.print("this is location of finding element ="+i);
+            if(arr[i]==key){
+               return i;
             }
         }
+        return -1;
     }
     public static void main(String[] args) {
         int arr[]={34,56,3,6,89,0};
-        int num=3;
-        linear(arr, num);
+        int key=6;
+        System.out.println("Index of Given Key : "+linear(arr, key));
     }
     
 }
