72789c4a1e843dbe4532ac708886218a2d4ac796
Sumit Kumar Sah <sumitkumar92365gmail.com>
2025-06-17 12:57:18 +0530

    Print Of Subarray

---
+++ Array/printSubarray.java
@@ -0,0 +1,22 @@
+public class printSubarray {
+    public static void subarray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            int start=i;
+            for(int j=i;j<arr.length;j++){
+                int end=j;
+
+               for(int k=start;k<=end;k++){
+                System.out.print(arr[k]+" ");
+               }
+               System.out.println();
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={2,4,6,8,10};
+        subarray(arr);
+
+    }
+    
+}
